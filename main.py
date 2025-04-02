4725929633f5b7e3a01d4cd7b2c27df09d88b749
theus.dev <matheusmnt13@outlook.com>
2025-04-02 16:28:55 -0300

    primeira versao do controle de estoque

---
+++ main.py
@@ -0,0 +1,12 @@
+# crie um sistema de controle de estoque de produtos
+
+produto1 = input("Digite o nome do primeiro produto: ")
+estoque1 = int(input(f"Quantos {produto1} você tem em estoque? "))
+
+produto2 = input("Digite o nome do segundo produto: ")
+estoque2 = int(input(f"Quantos {produto2} você tem em estoque? "))
+
+print("No estoque temos: ")
+print(f"{produto1} com {estoque1} itens")
+print(f"{produto2} com {estoque2} itens")
+
